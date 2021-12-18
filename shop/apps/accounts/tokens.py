65a1c0c05684d85ce0bb975c38da6cd70ce6882c
Yariquezz <yariquezz@gmail.com>
2021-12-18 13:54:41 +0200

    fix tokens

--- shop/apps/accounts/tokens.py
+++ shop/apps/accounts/tokens.py
@@ -5,8 +5,11 @@ import six
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) /
-            + six.text_type(user.is_active)
+            "%s%s%s" % (
+                six.text_type(user.pk),
+                six.text_type(timestamp),
+                six.text_type(user.is_active)
+            )
         )
 
 
