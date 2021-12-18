009d8bc94750c7850b3557b49274422a24eb0db5
Yariquezz <yariquezz@gmail.com>
2021-12-18 13:43:41 +0200

    fix flake8

--- shop/apps/accounts/urls.py
+++ shop/apps/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('register/', views.register_form, name='register'),
     path('logout/', views.logout_form, name='logout'),
     path(
-        'activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        'activate/(?P<uidb64>[0-9A-Za-z_]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         views.activate, name='activate'
     )
 ]
