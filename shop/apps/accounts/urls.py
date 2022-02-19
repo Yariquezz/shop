b5c29dabdf2d0fd97e956cd6248885421cc0366b
Yariquezz <yariquezz@gmail.com>
2022-02-19 15:23:30 +0200

    Create guest orders logic

--- shop/apps/accounts/urls.py
+++ shop/apps/accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.login_form, name='login'),
     path('register/', views.register_form, name='register'),
     path('logout/', views.logout_form, name='logout'),
-    path('activate/<uidb64>/(<token>/', views.activate, name='activate')
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate')
 ]
